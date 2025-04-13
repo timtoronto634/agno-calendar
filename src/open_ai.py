47409ee7698eeadb670726000530241e30f28dab
timtoronto634 <tim225x@gmail.com>
2025-04-13 13:11:24 +0900

    Refactor agent setup and enhance requirements; update .gitignore

--- src/open_ai.py
+++ src/open_ai.py
@@ -1,10 +1,31 @@
-from agno.agent import Agent, RunResponse
+from agno.agent import Agent
 from agno.models.openai import OpenAIChat
+from agno.playground import Playground, serve_playground_app
+from agno.storage.sqlite import SqliteStorage
+from agno.tools.duckduckgo import DuckDuckGoTools
 
-agent = Agent(
+
+agent_storage: str = "tmp/agents.db"
+
+web_agent = Agent(
+    name="Web Agent",
     model=OpenAIChat(id="gpt-4o"),
-    markdown=True
+    tools=[DuckDuckGoTools()],
+    instructions=["Always include sources"],
+    # Store the agent sessions in a sqlite database
+    storage=SqliteStorage(table_name="web_agent", db_file=agent_storage),
+    # Adds the current date and time to the instructions
+    add_datetime_to_instructions=True,
+    # Adds the history of the conversation to the messages
+    add_history_to_messages=True,
+    # Number of history responses to add to the messages
+    num_history_responses=5,
+    # Adds markdown formatting to the messages
+    markdown=True,
 )
 
-# Print the response in the terminal
-agent.print_response("hi.")
+
+app = Playground(agents=[web_agent]).get_app()
+
+if __name__ == "__main__":
+    serve_playground_app("open_ai:app", reload=True)
